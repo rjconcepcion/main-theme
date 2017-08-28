08188668523e204135a99b84993c2000f5904fe9
Robert <robertjohnconcepcion@gmail.com>
2017-08-28 18:59:34 +0800

    update message of 404page

--- 404.php
+++ 404.php
@@ -3,7 +3,8 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-					<h2>404 PAGE NOT FOUND</h2>
+					<h2>404 PAGE NOT FOUND :-(</h2>
+					<p>Better to back in <a href="<?php bloginfo('url'); ?>">home</a></p>
 				</div>
 				<div class="clearfix"></div>
 			</div> <!-- .row -->
