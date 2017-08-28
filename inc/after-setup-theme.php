1cd2f48d0a26174e5e3b2d867858a42940e801de
Robert <robertjohnconcepcion@gmail.com>
2017-08-28 19:22:43 +0800

    page support excerpt

--- inc/after-setup-theme.php
+++ inc/after-setup-theme.php
@@ -10,6 +10,7 @@ if(!function_exists("theme_setup")):
 		//add_theme_support( 'woocommerce' );
 		add_theme_support( 'title-tag' );
 		add_theme_support( 'post-thumbnails' );
+		add_post_type_support( 'page', 'excerpt' );
 		register_nav_menus(array("main" => "The main menu of the website"));
 
 		/* LIST OF DEFAULT URLS */
